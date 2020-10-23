1f712d19b631594414ac81e12b15241a4986e062
Gordon <72114656+GJEEL@users.noreply.github.com>
2020-10-22 21:21:54 -0500

    Created scatter.py

---
+++ src/scatter.py
@@ -0,0 +1,37 @@
+import logging
+
+from PySide2 import QtWidgets, QtCore
+from shiboken2 import wrapInstance
+import maya.OpenMayaUI as omui
+import maya.cmds as cmds
+import pymel.core as pmc
+from pymel.core.system import Path
+
+log = logging.getLogger(__name__)
+
+
+def maya_main_window():
+    """Returns the Maya main window widget"""
+    main_window = omui.MQtUtil.mainWindow()
+    return wrapInstance(long(main_window), QtWidgets.QWidget)
+
+
+class ScatterUI(QtWidgets.QDialog):
+    """Scatter Tool UI Class"""
+
+    def __init__(self):
+        """Constructor"""
+        super(ScatterUI, self).__init__(parent=maya_main_window())
+        self.setWindowTitle("Scatter Tool")
+        self.setWindowFlags(self.windowFlags() ^
+                            QtCore.Qt.WindowContextHelpButtonHint)
+        self.scenefile = SceneFile()
+        self.create_ui()
+        self.create_connections()
+
+    def create_ui(self):
+        """Creates UI widgets"""
+
+
+class ScatterTool():
+    """"""
